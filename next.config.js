5a2b8b50faf3c071ce2bb4faf0f3b43e3c146e0d
manish <manishshaw776@gmail.com>
2023-01-19 15:40:10 +0530

    new updates and files changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['Poppins'] } },
+    ],
+  },
+  images: {
+    domains: ['192.168.0.61'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { Poppins } from '@next/font/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AnimatePresence } from 'framer-motion';
+import { ContextProvider } from '../components/industries/ContextProvider';
+//internal imports
 
+const poppins = Poppins({
+  weight: '400',
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AnimatePresence exitBeforeEnter>
+        <ContextProvider>
+          <div className={poppins.className}>
+            <Component {...pageProps} />
+          </div>
+        </ContextProvider>
+      </AnimatePresence>
+    </QueryClientProvider>
+  );
 }
